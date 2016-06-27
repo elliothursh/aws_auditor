e3bf3f81f501ca5ddc13d9814094d5699ca2ffb2
Emma Sax <emma.sax4@gmail.com>
2016-06-27 15:08:53 -0500

    Updating commnent to be a little more clear

--- lib/aws_auditor/aws.rb
+++ lib/aws_auditor/aws.rb
@@ -14,7 +14,7 @@ module AwsAuditor
 
       iam = Aws::IAM::Client.new
 
-       # this will be an array of 1 because iam.list_mfa_devices.mfa_devices will only return 0 or 1 device per user;
+       # this will be an array of 0 or 1 because iam.list_mfa_devices.mfa_devices will only return 0 or 1 device per user;
        # if user doesn't have MFA enabled, then this loop won't even execute
       iam.list_mfa_devices.mfa_devices.each do |mfadevice|
         mfa_serial_number = mfadevice.serial_number
