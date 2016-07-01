bec209cb71c786c9ae84aadef95cb73314421811
matthewrkrieger <matt.krieger@sportngin.com>
2016-07-01 12:21:14 -0500

    Switch to using Hashie::Mash for config data

--- spec/aws_auditor/config_spec.rb
+++ spec/aws_auditor/config_spec.rb
@@ -4,44 +4,20 @@ require 'aws_auditor'
 module AwsAuditor
   describe Config do
 
-    context "#set_config_options" do
-      it "should create a corresponding method that returns the value" do
-        AwsAuditor::Config.set_config_options(test: "something")
-        expect(AwsAuditor::Config.test).to eql("something")
-      end
-
-      it "setting a new key should return the entire config hash" do
-        items = AwsAuditor::Config.config.count
-        expect(AwsAuditor::Config.set_config_options(test1: "something_else").count).to eql(items + 1)
-        expect(AwsAuditor::Config.set_config_options(test2: "something else").count).to eql(items + 2)
-      end
-    end
-
-    context "#config_data" do
-      it "should be a private method" do
-        expect(AwsAuditor::Config).not_to respond_to(:config_data)
-      end
-    end
-
     context "config key methods" do
       it "should return nil when not set" do
         expect(AwsAuditor::Config.doesnt_exist).to eql(nil)
+        expect(AwsAuditor::Config.doesnt_exist?).to eql(false)
       end
-
       it "should return the config value when set" do
-        AwsAuditor::Config.set_config_options(new_value: "testing")
+        AwsAuditor::Config.new_value = "testing"
         expect(AwsAuditor::Config.new_value).to eql("testing")
       end
     end
 
     context "after loading a config file" do
       before do
-        config_file = {"domain" => "example_domain",
-                       "slack" => {"slack_option"=>true,
-                                   "username" => "Rspec Tester",
-                                   "icon_url" => "http://fake.url",
-                                   "channel" => "#test-channel",
-                                   "webhook" => "https://slack.web.hook"}}
+        config_file = {"domain"=>"example_domain", "slack"=>{"slack_option"=>true, "username"=>"Rspec Tester", "icon_url"=>"http://fake.url", "channel"=>"#test-channel", "webhook"=>"https://slack.web.hook"}}
         allow(YAML).to receive(:load_file).and_return(config_file)
         allow(File).to receive(:exist?).and_return(true)
         AwsAuditor::Config.load("dummy/path")
@@ -55,9 +31,10 @@ module AwsAuditor
 
       it "overwriting values should work" do
         expect(AwsAuditor::Config.slack).to be_kind_of(Hash)
-        AwsAuditor::Config.set_config_options(slack: "this is a string now")
+        AwsAuditor::Config.slack = "this is a string now"
         expect(AwsAuditor::Config.slack).to eql("this is a string now")
       end
+
     end
   end
 end
