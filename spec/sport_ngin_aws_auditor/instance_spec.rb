412e79b38a438b2b2b18ed044d9e4f40c2d29477
Emma Sax <emma.sax4@gmail.com>
2016-09-26 10:59:30 -0500

    Passing all remaining tests

--- spec/sport_ngin_aws_auditor/instance_spec.rb
+++ spec/sport_ngin_aws_auditor/instance_spec.rb
@@ -3,26 +3,32 @@ require "sport_ngin_aws_auditor"
 module SportNginAwsAuditor
   describe Instance do
     it "should make a reserved instance with proper attributes" do
-      instance = Instance.new("Windows VPC us-east-1e m1.large", 4)
+      instance = Instance.new("Windows VPC us-east-1e m1.large", [4])
       expect(instance).to be_an_instance_of(Instance)
-      expect(instance.type).to eq("reserved")
+      expect(instance.category).to eq("reserved")
+      expect(instance.type).to eq("Windows VPC us-east-1e m1.large")
       expect(instance.count).to eq(4)
       expect(instance.tagged?).to eq(false)
       expect(instance.reserved?).to eq(true)
     end
 
     it "should make a running instance with proper attributes" do
-      instance = Instance.new("Windows VPC us-east-1e m1.large", -1)
+      instance = Instance.new("Windows VPC us-east-1e m1.large", [-1])
       expect(instance).to be_an_instance_of(Instance)
-      expect(instance.type).to eq("running")
+      expect(instance.category).to eq("running")
+      expect(instance.type).to eq("Windows VPC us-east-1e m1.large")
       expect(instance.count).to eq(-1.abs)
     end
 
     it "should make an instance with a tag with proper attributes" do
-      instance = Instance.new("Windows VPC us-east-1e m1.large with tag", 4)
+      instance = Instance.new("Windows VPC us-east-1e m1.large with tag", [4, 'example-instance-name', 'This is an example', '09/12/2015'])
       expect(instance).to be_an_instance_of(Instance)
-      expect(instance.type).to eq("tagged")
+      expect(instance.category).to eq("tagged")
+      expect(instance.type).to eq("Windows VPC us-east-1e m1.large")
       expect(instance.count).to eq(4)
+      expect(instance.name).to eq('example-instance-name')
+      expect(instance.reason).to eq('This is an example')
+      expect(instance.tag_value).to eq('09/12/2015')
       expect(instance.tagged?).to eq(true)
       expect(instance.running?).to eq(false)
     end
--- spec/sport_ngin_aws_auditor/recently_retired_tag_spec.rb
+++ spec/sport_ngin_aws_auditor/recently_retired_tag_spec.rb
@@ -2,13 +2,28 @@ require "sport_ngin_aws_auditor"
 
 module SportNginAwsAuditor
   describe RecentlyRetiredTag do
-    it 'should have an instance name as an instance_name' do
-      tag = RecentlyRetiredTag.new('09/01/2000', 'Linux VPC us-east-1b t2.small')
-      expect(tag.instance_name).to eq('Linux VPC us-east-1b t2.small')
+    it 'should have an instance name as an instance_type' do
+      tag = RecentlyRetiredTag.new('09/01/2000', 'Linux VPC us-east-1b t2.small', 'Tag name', 'This is an example')
+      expect(tag.instance_type).to eq('Linux VPC us-east-1b t2.small')
+    end
+
+    it 'should have a name as the instance_name' do
+      tag = RecentlyRetiredTag.new('09/01/2000', 'Linux VPC us-east-1b t2.small', 'Tag name', 'This is an example')
+      expect(tag.instance_name).to eq('Tag name')
+    end
+
+    it 'should have a string description as the reason' do
+      tag = RecentlyRetiredTag.new('09/01/2000', 'Linux VPC us-east-1b t2.small', 'Tag name', 'This is an example')
+      expect(tag.reason).to eq('This is an example')
+    end
+
+    it 'should have nil as the reason' do
+      tag = RecentlyRetiredTag.new('09/01/2000', 'Linux VPC us-east-1b t2.small', 'Tag name')
+      expect(tag.reason).to eq(nil)
     end
 
     it 'should have a string date as a value' do
-      tag = RecentlyRetiredTag.new('09/01/2000', 'Linux VPC us-east-1b t2.small')
+      tag = RecentlyRetiredTag.new('09/01/2000', 'Linux VPC us-east-1b t2.small', 'Tag name')
       expect(tag.value).to eq('09/01/2000')
     end
   end
